e2dc7714cf0cac8904e6eb972dd1f9b58a2e9e30
febbychang <febbychang2019@gmail.com>
2017-07-19 15:17:33 -0400

    Add files via upload

---
+++ imagefilter.py
@@ -0,0 +1,39 @@
+from PIL import Image
+
+# RGB values for recoloring.
+darkBlue = (0, 51, 76)
+red = (217, 26, 33)
+lightBlue = (112, 150, 158)
+yellow = (252, 227, 166)
+
+green = (74, 224, 69)
+darkGreen = (33, 135, 52)
+purple = (81, 5, 77)
+pink = (224, 100, 169)
+# Import image.
+my_image = Image.open("pic2.jpg")
+image_list = my_image.getdata() #each pixel is represented in the form (red value, green value, blue value, transparency). You don't need the fourth value.
+image_list = list(image_list) #Turns the sequence above into a list. The list can be iterated through in a loop.
+recolored = [] #list that will hold the pixel data for the new image.
+
+
+#YOUR CODE to loop through the original list of pixels and build a new list based on intensity should go here
+def color (darkcolor, mediumcolor, lightcolor, lightercolor):
+    for rgbvalue in image_list:
+        intensity= rgbvalue [0] + rgbvalue [1] + rgbvalue [2]
+        if intensity < 182:
+            recolored.append (darkcolor)
+        elif intensity < 364:
+            recolored.append (mediumcolor)
+        elif intensity < 546:
+            recolored.append (lightcolor)
+        else:
+            recolored.append (lightercolor)
+
+#color (darkBlue, red, lightBlue, yellow)
+color (purple, darkGreen, green, pink)
+# Create a new image using the recolored list. Display and save the image.
+new_image = Image.new("RGB", my_image.size) #Creates a new image that will be the same size as the original image.
+new_image.putdata(recolored) #Adds the data from the recolored list to the image.
+new_image.show() #show the new image on the screen
+new_image.save("recolored.jpg", "jpeg") #save the new image as "recolored.jpg"
